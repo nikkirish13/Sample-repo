c5f4bb9e5c3ed0cddecb33f740ba73e336c665f3
nikkirish13 <44093894+nikkirish13@users.noreply.github.com>
2019-01-22 23:28:53 +0530

    Add files via upload

---
+++ no_of_sentences.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+    char str[100];
+    int c=1, i;
+    gets(str);
+
+    while(str[i]!='\0')
+    {
+        if(str[i]=='.')
+        {
+            c++;
+        }
+        
+        i++;
+    }
+
+    printf("%d",c);
+
+    return 0;
+}
